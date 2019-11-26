e47269c527a06ef4b38fc3b9e5d3e8b5599004ed
zhang <1065859745@qq.com>
2019-11-26 11:44:41 +0800

    New branch udp-1.1.0

--- call.go
+++ call.go
@@ -14,37 +14,14 @@ import (
 	"time"
 )
 
-type Call struct {
-	port       *string
-	udpAddress net.UDPAddr
-	messages   string
-	all, to    []net.UDPAddr
-}
-
-var call = Call{port: flag.String("p", "", "Local serve port, default random.")}
+var port, all = flag.String("p", "", "Local serve port, default random."), []net.UDPAddr{}
+var messageCh, downCh, answerCh chan string
 
 func main() {
 	flag.Parse()
-	scan := bufio.NewScanner(os.Stdin)
+	scan, messages, name := bufio.NewScanner(os.Stdin), ``, `[Unkown]`
 	reg, _ := regexp.Compile(`^\s*$`)
 
-	if *call.port == `` {
-		for {
-			rand.Seed(time.Now().Unix())
-			p := strconv.Itoa(rand.Intn(59000) + 1000)
-			*call.port = p
-			udpAddr, _ := net.ResolveUDPAddr(`udp`, ":"+*call.port)
-			l, e := net.ListenUDP("udp", udpAddr)
-			if e != nil {
-				continue
-			} else {
-				l.Close()
-			}
-			fmt.Printf("Serve at 127.0.0.1:%s\n", *call.port)
-			call.udpAddress = *udpAddr
-			break
-		}
-	}
 	go serve()
 
 	for {
@@ -52,102 +29,129 @@ func main() {
 		t := scan.Text()
 		if !reg.MatchString(t) {
 			if strings.HasPrefix(t, `$Call`) {
-				to := strings.Fields(t)[1:]
-				delSame(&to, `127.0.0.1:`+*call.port)
-				c := []net.UDPAddr{}
-				for _, v := range to {
-					addr, e := net.ResolveUDPAddr(`udp`, v)
-					if e != nil {
-						log.Printf("| %s ", e.Error())
-						continue
-					}
-					c = append(c, *addr)
-				}
-				call.to = c
+
 				continue
 			}
-			if strings.HasPrefix(t, `$Clear all`) {
-				call.all = []net.UDPAddr{}
+			if strings.HasPrefix(t, `$Name`) {
+
 				continue
 			}
 			if strings.HasPrefix(t, `$List`) {
-				fmt.Println(`-------------------------`)
-				for i, v := range call.all {
-					fmt.Println(i+1, v)
-				}
-				fmt.Println(`-------------------------`)
+
 				continue
 			}
-			if strings.HasPrefix(t, `$All`) {
-				call.to = []net.UDPAddr{}
+			if strings.HasPrefix(t, `$Answer`) {
+
 				continue
 			}
 
-			call.messages = t
-			if len(call.to) != 0 {
-				go call.Send(call.to)
-				continue
-			}
-			go call.Send(call.all)
+			messages = fmt.Sprintf("%s : %s", name, t)
+			messageCh <- messages
+			<-messageCh
 		}
 	}
 }
 
 func serve() {
-	conn, _ := net.ListenUDP("udp", &call.udpAddress)
+	var conn *net.UDPConn
+	var udpAddr *net.UDPAddr
+	var e error
+
+	if *port == `` {
+		for {
+			rand.Seed(time.Now().Unix())
+			*port = strconv.Itoa(rand.Intn(59000) + 1000)
+			udpAddr, _ = net.ResolveUDPAddr(`udp`, ":"+*port)
+			conn, e = net.ListenUDP("udp", udpAddr)
+			if e != nil {
+				continue
+			}
+			fmt.Printf("Serve at 127.0.0.1:%s\n", *port)
+			break
+		}
+	} else {
+		udpAddr, e = net.ResolveUDPAddr(`udp`, ":"+*port)
+		if e != nil {
+			log.Fatal(e)
+		}
+		conn, _ = net.ListenUDP("udp", udpAddr)
+	}
+
+	addUDP(&all, udpAddr.String())
+
+	go read(conn)
+
+	write(conn)
+}
+
+func client(udpAddr *net.UDPAddr) {
+	conn, _ := net.DialUDP("udp", nil, udpAddr)
+
+	addUDP(&all, udpAddr.String())
+
+	go read(conn)
+
+	write(conn)
+}
+
+func read(conn *net.UDPConn) {
+	remoteAddr := conn.RemoteAddr().String()
 	for {
 		var b [512]byte
 		n, addr, e := conn.ReadFromUDP(b[:])
 		if e != nil {
-			log.Printf("| %s", e.Error())
+			downCh <- `$Down ` + remoteAddr
+			conn.Close()
+			delUDPAddr(&all, remoteAddr)
+			<-downCh
 			return
 		}
 
 		m := string(b[:n])
-		remoteAddr := fmt.Sprintf("%s:%s", addr.IP, strings.Split(m, ` `)[0])
-
-		if remoteAddr != fmt.Sprintf("127.0.0.1:%s", *call.port) {
-			log.Printf("|From| %s:%s", addr.IP, m)
-			if e = addUDP(&call.all, remoteAddr); e != nil {
-				log.Printf("| %s", e.Error())
-			}
-		}
+		log.Printf("|From| %s %s", addr.String(), m)
+		log.Printf("| %s", e.Error())
 	}
 }
 
-func (c Call) Send(a []net.UDPAddr) {
-	for _, v := range a {
-		udpAddr := v.String()
-		conn, e := net.Dial("udp", udpAddr)
+func write(conn *net.UDPConn) {
+	var m string
+	remoteAddr := conn.RemoteAddr().String()
+
+	for {
+		select {
+		case m = <-messageCh:
+			messageCh <- m
+		case m = <-downCh:
+			downCh <- m
+		}
+
+		if strings.HasPrefix(m, "$Answer "+remoteAddr) {
+			m = <-answerCh
+			answerCh <- m
+		} else if strings.HasPrefix(m, "$Down "+remoteAddr) || strings.HasPrefix(m, "$Down all") {
+			conn.Close()
+			break
+		}
+
+		_, e := conn.Write([]byte(m))
 		if e != nil {
 			log.Printf("| %s", e.Error())
-			continue
-		}
-
-		log.Printf("|To| %s", c.messages)
-		conn.Write([]byte(fmt.Sprintf("%s : %s", *c.port, c.messages)))
-		conn.Close()
-
-		if udpAddr != `127.0.0.1`+*c.port {
-			if e = addUDP(&call.all, udpAddr); e != nil {
-				log.Printf("| %s", e.Error())
-				return
-			}
+			break
 		}
 	}
 }
 
-func delSame(ar *[]string, s string) {
-	arr := *ar
+func delSame(arr *[]string) {
+	ar := *arr
 a:
 	for {
-		for i, n := range arr {
-			if i != len(arr)-1 {
-				for j, m := range arr[i+1:] {
+		for i, n := range ar {
+			if i != len(ar)-1 {
+				for j, m := range ar[i+1:] {
 					if n != m {
 						continue
 					} else {
-						arr = append(arr[:j+i+1], arr[j+i+2:]...)
+						ar = append(ar[:j+i+1], ar[j+i+2:]...)
 						continue a
 					}
 				}
@@ -155,22 +159,24 @@ a:
 		}
 		break
 	}
-
-	for i, v := range arr {
-		if v != s {
-			continue
-		}
-		arr = append(arr[:i], arr[i+1:]...)
-		break
-	}
-	*ar = arr
+	*arr = ar
 }
 
-func addUDP(u *[]net.UDPAddr, s string) error {
-	udpAddr, e := net.ResolveUDPAddr(`udp`, s)
-	if e != nil {
-		return e
+func delUDPAddr(arr *[]net.UDPAddr, s string) {
+	ar := *arr
+	for i, v := range ar {
+		if v.String() != s {
+			continue
+		}
+		ar = append(ar[:i], ar[i+1:]...)
+		break
 	}
+	*arr = ar
+}
+
+func addUDP(u *[]net.UDPAddr, s string) {
+	udpAddr, _ := net.ResolveUDPAddr(`udp`, s)
+
 	if len(*u) != 0 {
 		for i, v := range *u {
 			if i != len(*u)-1 {
@@ -186,5 +192,4 @@ func addUDP(u *[]net.UDPAddr, s string) error {
 	} else {
 		*u = append(*u, *udpAddr)
 	}
-	return e
 }
